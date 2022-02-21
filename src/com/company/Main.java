9c6812044b469c4b0adde4e8626c36af6a4cf858
Brennast26 <brennast@mail.uc.edu>
2022-02-21 13:30:25 -0500

    Lab 4 task 1-Sales Tax

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	double priceofpurchase = 12.50;
+    System.out.println("The price of your purchase $"+priceofpurchase);
+    double afrersalestax = priceofpurchase * 1.05;
+    System.out.println("Your total after sales tax is $"+afrersalestax);
     }
 }
